4c2734fe38d98a862db88e4d8bb9ad879c463d6e
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-11-19 22:16:56 -0600

    Edited url on API.js

--- employee-directory-app/src/utils/API.js
+++ employee-directory-app/src/utils/API.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=800";
+const BASEURL = "https://randomuser.me/api/?results=100&nat=us";
 
 export default {
     search: function() {
         return axios.get(BASEURL);
     }
-}
+};
