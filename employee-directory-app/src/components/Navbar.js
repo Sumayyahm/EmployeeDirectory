d4ef8023855ae10d2a3a0d6521a48ce1c027e464
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-11-19 22:06:51 -0600

    Added a Navbar Component

---
+++ employee-directory-app/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function Navbar() {
+    return (
+<div className="navbar">
+  <div>
+    <h1 className="h1tag">Employee Directory</h1>
+    <p className="ptag">Click on the Name heading to sort by Name or use search box to narrow your results</p>
+  </div>
+</div>
+    )
+}
+
+export default Navbar;
